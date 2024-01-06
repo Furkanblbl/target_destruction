38e4c24836b89121205bde2be777fe2a3513632f
doga66 <dodouzl126@gmail.com>
2024-01-06 16:25:45 +0300

    center.difference

--- main.py
+++ main.py
@@ -57,3 +57,40 @@ class ROVController:
         if status != sl.ERROR_CODE.SUCCESS: # Check that the camera is opened
             print(repr(status))
             exit()
+        
+        try:
+            while True: # Main loop
+                runtime_parameters = sl.RuntimeParameters() # Create a RuntimeParameters object for camera parameters
+                left_image = sl.Mat() # Create sl.Mat objects for left images
+                right_image = sl.Mat() # Create sl.Mat objects for right images
+
+                if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS: # Grab an image from the camera
+                    zed.retrieve_image(left_image, sl.VIEW.LEFT) 
+                    zed.retrieve_image(right_image, sl.VIEW.RIGHT) 
+
+                    frame = cv2.cvtColor(left_image.get_data(), cv2.COLOR_BGR2RGB) # Convert the image to RGB
+
+                    df_result = self.image_processor.process_frame(frame) # Process the image
+                    
+
+                    if df_result is not None and not df_result.empty: # If there is a result from the image processing algorithm 
+                        for ind in df_result.index: # For each object detected
+                            if df_result['class'][ind] == 0 and df_result['confidence'][ind] >= 0.5: # If the object is a red circle and the confidence is greater than 0.5
+                                x1, y1 = int(df_result['xmin'][ind]), int(df_result['ymin'][ind]) # Get the coordinates of the object
+                                x2, y2 = int(df_result['xmax'][ind]), int(df_result['ymax'][ind]) # Get the coordinates of the object
+
+                                noktax = int(((x2+x1)/2)) 
+                                noktay = int((y2+y1)/2) 
+                                nokta = noktax,noktay # get the center of the object
+                                
+                                alan = (x2-x1) * (y2-y1) # get the area of the object
+                                
+                                farkx_ = 320 - noktax # get the difference between the center of the object and the center of the image
+                                farky_ = 240 - noktay # get the difference between the center of the object and the center of the image
+                            
+                                label = df_result['name'][ind] # get the label of the object
+                                conf =  df_result['confidence'][ind] # get the confidence of the object
+                                text = label + " " + str(conf.round(decimals=2)) # get the text to be written on the image
+                                cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,0),2) # draw a rectangle around the object
+                                cv2.putText(frame, text,(x1,y1-5), cv2.FONT_ITALIC,2 ,(0,0,0),2) # write the text on the image
+                                cv2.putText(frame, ".", (nokta), cv2.FONT_ITALIC, 1, (0, 0, 255), 4)
